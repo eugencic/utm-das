6cd4b8b7d761d39287896e44c0a2f45411eb9cf0
katko23 <catalin.coseru@isa.utm.md>
2023-01-23 13:08:31 +0200

    Add the right file with add presence

--- backend/DBfiles/AddPresence.py
+++ backend/DBfiles/AddPresence.py
@@ -65,3 +65,31 @@ def exitSchool(idnp, data):
         db.close()
 
     return ans
+
+def prezent_liceu(idnp, data):
+
+    db = newConnect()
+    cur = db.cursor()
+    ans = 200
+    try:
+        cur.execute("SELECT id_elev FROM sql7588695.elevi WHERE (`idnp` = '" + idnp + "')")
+        id_copil = cur.fetchall()[0][0]
+
+        cur.execute("SELECT `" + data + "` FROM sql7588695.prezenta_liceu WHERE `id_elev` = '" + str(id_copil) + "'")
+        prezenta = cur.fetchall()[0][0]
+
+        if(prezenta == 'p'):
+            cur.execute("UPDATE sql7588695.prezenta_liceu SET `" + data + "` = 'e'"
+                        "where ( `id_elev` = '" + str(id_copil) + "');")
+        else:
+            cur.execute("UPDATE sql7588695.prezenta_liceu SET `" + data + "` = 'p'"
+                        "where ( `id_elev` = '" + str(id_copil) + "');")
+        db.commit()
+    except Error as err:
+        print(err)
+        ans = 404
+    finally:
+        cur.close()
+        db.close()
+
+    return ans
